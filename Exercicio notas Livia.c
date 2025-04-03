b28dd72d3a3b184693ce5d7a067d461047c76f87
livia <liviamajzoubsilva19@gmail.com>
2025-04-03 16:48:09 -0300

    Exercicio notas Livia

---
+++ Exercicio notas Livia.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+    float n1, n2, n3, n4,
+          media;
+
+    printf("Digite as quatro notas: ");
+    scanf("%f %f %f %f", &n1,&n2,&n3,&n4);
+
+    media = (n1 + n2 + n3 + n4) / 4;
+
+    if (media >8)
+       printf("Aprovado com distinção. ");
+    if else (media >=6)
+       printf("Aprovado direto. ");
+    if else (media >=4)
+       printf("Vai para final. ");
+    else (media <4)
+       printf("Reprovado direto. ")
+       return 0;
+}
